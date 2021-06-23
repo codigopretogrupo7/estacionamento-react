26823ae3fff7b62d7c2a20c94733c6ab5401f17e
Lucas Damasceno <lucasd.viana1993@gmail.com>
2021-06-22 23:37:53 -0300

    Adicionando Linkagem da pagina vagas

--- src/componentes/Topo/index.js
+++ src/componentes/Topo/index.js
@@ -8,13 +8,18 @@ const Topo = () => {
     <header className="topo">
       <div className="topo-conteudo">
         <Link className="topo-logo-box" to="/">
-          <img className="topo-logo" src="assets/park.png" alt="logo estacionamento" title="logo estacionamento"/>
-          <p className="topo-nome-empresa">Lorem</p>
+          <img 
+            className="topo-logo" 
+            src="assets/park.png" 
+            alt="logo estacionamento" 
+            title="logo estacionamento"/>
+          <p className="topo-nome-empresa">Tem Vaga</p>
         </Link>
 
         <nav className="topo-links">
           <Link className="topo-link" to="/">Home</Link>
           <Link className="topo-link" to="/Estacionamentos">Estacionamentos</Link>
+          <Link className="topo-link" to="/Vagas">Vagas</Link>
           <Link className="topo-link" to="/contato">Contato</Link>
           <Link className="topo-link" to="/login">Login</Link>
         </nav>
