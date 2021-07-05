c9aac80e73d0965c016e473b35bdf6a07f85225e
Marcos André Ribeiro Gomes <marcosandrebahia@gmail.com>
2021-07-05 19:41:50 -0300

    Populando Cards

--- src/componentes/Carrossel/index.js
+++ src/componentes/Carrossel/index.js
@@ -58,46 +58,51 @@ class Carrossel extends Component{
           titulo='Estrela estacionameto' rua = 'Rua Campo Novo'
           bairro = 'Pinheiros' cidade='São Paulo'
           diaria = '45,00 '
-          descricao='Estacionamento coberto.'
+          descricao='Vaga coberta.'
           />
         
           <Card
           imagem='assets/estacionamentos/estacionamento2.jpg' 
-          titulo='Estrela estacionameto' rua = 'Rua Campo Novo'
-          bairro = 'Pinheiros' cidade='São Paulo'
-          diaria = '45,00 '
-          descricao='Estacionamento coberto.'
+          titulo='Inpark estacionameto' rua = 'Rua salvador'
+          bairro = 'Zona Norte' cidade='São Paulo'
+          diaria = '40,00 '
+          descricao='Vaga coberta.'
           />
           <Card
-          imagem='assets/estacionamentos/estacionamento1.jpg' estacionamento='Estacionamento1' 
-          titulo='Teste' descricao='Lorem Lorem'
-          bairro = 'Pinheiros' cidade='São Paulo'
-          diaria = '45,00 '
-          descricao='Estacionamento coberto.'/>
+          imagem='assets/estacionamentos/estacionamento1.jpg' 
+          titulo='Estapar' rua = 'Rua Arcipreste Paiva'
+          bairro = 'São Domingos' cidade='São Paulo'
+          diaria = '35,00 '
+          descricao='Vaga coberta.'
+          />
           <Card
-          imagem='assets/estacionamentos/estacionamento3.jpg' estacionamento='Estacionamento1' 
-          titulo='Teste1' descricao='Lorem Lorem'
-          bairro = 'Pinheiros' cidade='São Paulo'
-          diaria = '45,00 '
-          descricao='Estacionamento coberto.'/>
+          imagem='assets/estacionamentos/estacionamento3.jpg' 
+          titulo='MultiPark Itaberaba' rua = 'Rua Jaguabara'
+          bairro = 'Itaberaba' cidade='São Paulo'
+          diaria = '35,00 '
+          descricao='Vaga coberta.'
+          />
           <Card
-          imagem='assets/estacionamentos/estacionamento4.jpg' estacionamento='Estacionamento1' 
-          titulo='Teste1' descricao='Lorem Lorem'
-          bairro = 'Pinheiros' cidade='São Paulo'
-          diaria = '45,00 '
-          descricao='Estacionamento coberto.'/>
+          imagem='assets/estacionamentos/estacionamento4.jpg' 
+          titulo='Campo de Marte' rua = 'Aeroporto Campo de Marte'
+          bairro = 'Rodovia Helio smith' cidade='São Paulo'
+          diaria = '55,00 '
+          descricao='Vaga descoberta.'
+          />
           <Card
-          imagem='assets/estacionamentos/estacionamento5.png' estacionamento='Estacionamento1' 
-          titulo='Teste1' descricao='Lorem Lorem'
-          bairro = 'Pinheiros' cidade='São Paulo'
-          diaria = '45,00 '
-          descricao='Estacionamento coberto.'/>
+         imagem='assets/estacionamentos/estacionamento5.png' 
+         titulo='Estapar' rua = 'Rua Coroneu Garcia'
+         bairro = 'Jardins' cidade='São Paulo'
+         diaria = '35,00 '
+         descricao='Vaga coberta.'
+         />
           <Card
-          imagem='assets/estacionamentos/estacionamento6.jpg' estacionamento='Estacionamento1' 
-          titulo='Teste1' descricao='Lorem Lorem'
-          bairro = 'Pinheiros' cidade='São Paulo'
-          diaria = '45,00 '
-          descricao='Estacionamento coberto.'/>
+          imagem='assets/estacionamentos/estacionamento6.jpg' 
+          titulo='Shopping estação' rua = 'Avenida Miguel Sutil'
+          bairro = 'Goiabeiras' cidade='São Paulo'
+          diaria = '35,00 '
+          descricao='Vaga coberta.'
+          />
         </Slider>
       </div>
       </div>
